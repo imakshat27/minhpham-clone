61b6fead7af53a0f79338c728a68350d73f2c66a
Akshat Agarwal <agarwalakshat2710@gmail.com>
2025-05-18 15:59:56 +0530

    add hero text style and favicon

--- components/HeroText.tsx
+++ components/HeroText.tsx
@@ -1,16 +1,17 @@
 export default function HeroText() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen text-center relative">
-      <p className="text-sm tracking-widest text-gray-400 uppercase mb-4">Minh Pham</p>
-      
-      <h1 className="text-6xl md:text-8xl font-extrabold leading-tight text-center">
-  <span className="block text-[rgb(183,171,152)]">MAKING</span>
-  <span className="block text-[rgb(235,89,57)]">GOOD</span>
-  <span className="block text-[rgb(235,89,57)]">SHIT</span>
-  <span className="block text-[rgb(183,171,152)]">SINCE</span>
-  <span className="block text-[rgb(183,171,152)]">2009</span>
-</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen text-center px-4">
+      <p className="text-sm sm:text-base tracking-widest text-[rgb(183,171,152)] uppercase mb-6">
+        Minh Pham
+      </p>
 
+      <h1 className="text-5xl sm:text-6xl md:text-8xl lg:text-9xl xl:text-10xl font-bold leading-[0.9] tracking-tight">
+        <span className="block text-[rgb(183,171,152)]">MAKING</span>
+        <span className="block text-[rgb(235,89,57)]">GOOD</span>
+        <span className="block text-[rgb(235,89,57)]">SHIT</span>
+        <span className="block text-[rgb(183,171,152)]">SINCE</span>
+        <span className="block text-[rgb(183,171,152)]">2009</span>
+      </h1>
     </div>
   )
 }
